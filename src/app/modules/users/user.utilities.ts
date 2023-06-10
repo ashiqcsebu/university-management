215a356f7e94d7aa7561b85128a6ec53e0b9ad1d
ashiqcsebu <ashiq.cse3.bu@gmail.com>
2023-06-10 23:25:08 +0600

    [DONE] Setup Logger

--- src/app/modules/users/user.utilities.ts
+++ src/app/modules/users/user.utilities.ts
@@ -1,4 +1,4 @@
-import { User } from './users.model'
+import { User } from './user.model'
 
 export const findLastUserId = async () => {
   const lastUser = await User.findOne({}, { id: 1, _id: 0 })
@@ -9,6 +9,6 @@ export const findLastUserId = async () => {
 
 export const generateUserId = async () => {
   const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0')
-  const incrementedId = (parseInt(currentId)+1).toString().padStart(5,'0');
-  return incrementedId;
+  const incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0')
+  return incrementedId
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,13 @@
+/* eslint-disable no-undef */
 import dotenv from 'dotenv'
 import path from 'path'
 
 dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
+  // eslint-disable-next-line no-undef
+  env: process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
-  default_student_pass : process.env.DEFAULT_USER_PASS
+  default_student_pass: process.env.DEFAULT_USER_PASS,
 }
